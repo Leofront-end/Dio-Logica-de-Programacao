1c2c76aa00f9e0e6531a11cfdcc3bfb80a1cc814
Leofront-end <ls171258@gmail.com>
2024-10-30 19:20:19 -0300

    variavel - let e const

---
+++ variaveis/vovo.js
@@ -0,0 +1,11 @@
+let poteCafe = "café pilão"
+let poteAcucar = "Açúcar cristal"
+let poteBiscoito = "Biscoitos Maizena"
+
+const mensagemDaVovo = "Na cozinha da vovó hoje tem: "
+
+console.log(mensagemDaVovo + poteCafe + " - " + poteAcucar + " - " + poteBiscoito)
+
+poteCafe = "Café 3 corações"
+
+console.log(mensagemDaVovo + poteCafe + " - " + poteAcucar + " - " + poteBiscoito)

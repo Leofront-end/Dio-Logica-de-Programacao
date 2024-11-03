2ccb8fe6ff2b20e9692e0c5725a9501455730bf6
Leofront-end <ls171258@gmail.com>
2024-11-03 08:26:47 -0300

    Update funcao2.js

--- funcao/funcao2.js
+++ funcao/funcao2.js
@@ -1,5 +1,9 @@
-torrar("pao de forma")
-torrar("pao integral")
-function torrar(pao) {
+torrar("pao de forma",  "leo", 10.90 )
+torrar("pao integral" , "leo")
+
+function torrar(pao, nome = "Cliente" , valor) {
     console.log("torrada feita com " + pao)
+    console.log("ela é um pedido de " + nome)
+    console.log("o valor total é " + valor)
 }
+
